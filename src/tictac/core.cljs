b051019b62d5a3804be63e80e82933dd0113f08a
Benjamin Teuber <benjamin.teuber@freiheit.com>
2015-11-23 19:28:55 +0100

    Some pre-structuring

--- src/tictac/core.cljs
+++ src/tictac/core.cljs
@@ -2,7 +2,33 @@
   (:require
    [reagent.core :as r])
   (:require-macros
-   [devcards.core :refer [defcard-rg deftest]]
+   [devcards.core :refer [defcard-rg defcard-doc deftest]]
    [cljs.test :refer [is]]))
 
 (enable-console-print!)
+
+(defcard-doc
+  "# Tic Tac Toe
+   Happy hacking!")
+
+;; Logic
+;; Hier kommt side-effect-freie Logik hin
+
+
+;; State
+;; Hier wird der app-state verwaltet
+
+
+;; Components
+;; Hier definieren wir reagent-components
+
+
+;; Tests
+;; Platz für ein paar Unit tests
+
+
+;; Cards
+;; Hier die devcards
+
+(defcard-rg hallo
+  [:p "Hallo Welt"])
